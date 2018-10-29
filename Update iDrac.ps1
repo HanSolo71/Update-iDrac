4452bf50434e2f2b91516799fc8da63e00ed5c11
HanSolo71 <HanSolo552@gmail.com>
2018-10-29 13:13:44 -0400

    Add files via upload

---
+++ Update iDrac.ps1
@@ -0,0 +1,9 @@
+﻿#List of iDrac Servers You Want To Update
+$servers = Get-Content "\\fileshare\idracs.txt"
+
+#To get the base configuration run "racadm.exe" -r SERVERFQDN/IP -u "Username" -p "Password" get -f C:\Path\To\Save\Config\Config.txt
+
+$servers | foreach
+ {
+ invoke-expression "racadm.exe -r $_ -u Username -p Password set -f C:\Path\To\Save\Config\Config.txt"
+ }
